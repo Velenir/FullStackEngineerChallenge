3db8b0779866a6a5626c3095ee8e293439e3a27b
Velenir <velenir@gmail.com>
2020-07-05 14:15:26 +0300

    allow to addUser

--- src/server/db/resolvers/UserResolver.ts
+++ src/server/db/resolvers/UserResolver.ts
@@ -1,5 +1,20 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Mutation, InputType, Field, Arg } from 'type-graphql';
 import { User } from '../entity/User';
+
+@InputType()
+class AddUserRequest {
+  @Field()
+  firstName: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -10,6 +25,21 @@ export class UserResolver {
 
   @Query(() => [User])
   users(): Promise<User[]> {
+    return User.find();
+  }
+
+  @Mutation(() => [User])
+  async addUser(@Arg('newUser') newUser: AddUserRequest): Promise<User[]> {
+    console.log('UserResolver::addUser', newUser);
+
+    // TODO: add hashing,jwt
+    const hashedPassword = newUser.password;
+
+    await User.insert({
+      ...newUser,
+      password: hashedPassword,
+    });
+
     return User.find();
   }
 }
