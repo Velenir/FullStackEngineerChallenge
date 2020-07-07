94a51a665fc92f0150df5234659b7535c0ee5d53
Velenir <velenir@gmail.com>
2020-07-07 18:54:14 +0300

    more comments

--- src/server/db/connectionOptions.ts
+++ src/server/db/connectionOptions.ts
@@ -1,13 +1,15 @@
 import { User, Review } from './entity';
 import { ConnectionOptions } from 'typeorm';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 // because of next.js webpack config
 // we can't use ormconfig.json
 // or we'll get `can't import outside of module` error
 export const DEFAULT_CONNECTION: ConnectionOptions = {
   type: 'sqlite',
   database: 'reviews-db.sqlite',
-  synchronize: true,
-  logging: true,
+  synchronize: isDevelopment, // synchronize DB entities on HMR
+  logging: isDevelopment, // log DB queries
   entities: [User, Review],
 };
